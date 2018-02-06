6fdb99d72474dff7b6e7d12258393a8d9968d693
paulj <jaffrepaul@gmail.com>
2018-02-06 17:15:37 -0500

    tooling setup

---
+++ src/api/__tests__/github.test.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+
+const github = require('../github')
+
+// A simple example test
+describe('#getUser() using Promises', () => {
+    it('should load user data', () => {
+        return github.getUser('vnglst')
+            .then(data => {
+                expect(data).toBeDefined()
+                expect(data.entity.name).toEqual('Koen van Gilst')
+            })
+    })
+})
